b2f8cbfd9d7ad9084938071f0c5142f4ebbe38a2
Hanna Sampaio <130690107+hannasampaio@users.noreply.github.com>
2025-04-08 17:35:51 +0100

    Adiciona tarefa de cálculo de média

---
+++ tarefa_calculo_media/src/CalculoMediaSimples.java
@@ -0,0 +1,16 @@
+/**
+ * @author sampaioh
+ */
+public class CalculoMediaSimples {
+    public static void main(String[] args) {
+        int nota1 = 7;
+        int nota2 = 8;
+        int nota3 = 9;
+        int nota4 = 6;
+
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Notas: " + nota1 + ", " + nota2 + ", " + nota3 + ", " + nota4);
+        System.out.println("A média é " + media);
+    }
+}
